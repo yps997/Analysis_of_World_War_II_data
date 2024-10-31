78fd456316e639cd4dfff3eae9ed6a80d6933356
yps997 <yairps997@gmail.com>
2024-10-31 15:41:28 +0200

    fixing models and add Lazy to relationships

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, Numeric, Date, DateTime
+from sqlalchemy import Column, Integer, String, Numeric, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from .database import Base
 
@@ -6,45 +6,41 @@ class Country(Base):
     __tablename__ = 'countries'
     country_id = Column(Integer, primary_key=True)
     country_name = Column(String(100), unique=True, nullable=False)
-    cities = relationship('City', backref='country', cascade="all, delete-orphan")
+    cities = relationship('City', backref='country', lazy=False)
 
 class City(Base):
     __tablename__ = 'cities'
     city_id = Column(Integer, primary_key=True)
-    city_name = Column(String(100), nullable=False)
+    city_name = Column(String(100))
     country_id = Column(Integer, ForeignKey('countries.country_id'), nullable=False)
-    targets = relationship('Target', backref='city')
+    latitude = Column(Numeric)
+    longitude = Column(Numeric)
+    targets = relationship('Target', backref='city', lazy=False)
 
 class TargetType(Base):
     __tablename__ = 'targettypes'
     target_type_id = Column(Integer, primary_key=True)
     target_type_name = Column(String(255), unique=True, nullable=False)
-    targets = relationship('Target', backref='target_type')
+    targets = relationship('Target', backref='target_type', lazy=False)
 
 class Target(Base):
     __tablename__ = 'targets'
     target_id = Column(Integer, primary_key=True)
-    mission_id = Column(Integer, ForeignKey('missions.mission_id', ondelete='CASCADE'))
+    mission_id = Column(Integer, ForeignKey('missions.mission_id'))
     target_industry = Column(String(255), nullable=False)
     city_id = Column(Integer, ForeignKey('cities.city_id'), nullable=False)
     target_type_id = Column(Integer, ForeignKey('targettypes.target_type_id'))
     target_priority = Column(Integer)
 
-class AttackType(Base):
-    __tablename__ = 'attack_types'
-    attack_type_id = Column(Integer, primary_key=True)
-    attack_type_name = Column(String(100), nullable=False)
-    missions = relationship('Mission', backref='attack_type')
-
 class Mission(Base):
     __tablename__ = 'missions'
     mission_id = Column(Integer, primary_key=True)
-    mission_date = Column(Date, nullable=False)
-    attack_type_id = Column(Integer, ForeignKey('attack_types.attack_type_id'))
+    mission_date = Column(Date)
+    airborne_aircraft = Column(Numeric(10,2))
     attacking_aircraft = Column(Numeric(10,2))
     bombing_aircraft = Column(Numeric(10,2))
     aircraft_returned = Column(Numeric(10,2))
     aircraft_failed = Column(Numeric(10,2))
     aircraft_damaged = Column(Numeric(10,2))
     aircraft_lost = Column(Numeric(10,2))
-    targets = relationship('Target', backref='mission', cascade="all, delete-orphan")
+    targets = relationship('Target', backref='mission', lazy=False)
